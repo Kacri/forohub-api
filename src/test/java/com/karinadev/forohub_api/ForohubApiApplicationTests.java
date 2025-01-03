0418967a60687472e1d8a6c5f28d4f331550916b
Kacri <karina.ghcan@gmail.com>
2025-01-03 11:48:31 -0500

    Inicializando foro hub

---
+++ src/test/java/com/karinadev/forohub_api/ForohubApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karinadev.forohub_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForohubApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
